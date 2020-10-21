b6feebb49e88867c806ad9deeabba93036818b8e
leanhtuan220301 <72974617+leanhtuan220301@users.noreply.github.com>
2020-10-21 10:42:07 +0700

    Add files via upload

---
+++ tuấn/tuấn/tuan.js
@@ -0,0 +1,49 @@
+    var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    var swiper = new Swiper('.swiper-container1', {
+      effect: '',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+      },
+
+      loop:true,
+      navigation: {
+      nextEl: '.nutnhan2',
+        prevEl: '.nutnhan1',
+      },
+    });
+
+    var swiper = new Swiper('.swiper-container2', {
+      effect: '',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+      },
+
+      loop:true,
+      navigation: {
+      nextEl: '.nutnhan2',
+        prevEl: '.nutnhan1',
+      },
+    });
